208277b7d7d9eec5c4b5f692a8624f09fb299d49
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2024-08-28 23:01:50 +0100

    fixed bugs in lexer

--- src/lexer.zig
+++ src/lexer.zig
@@ -5,7 +5,7 @@ const token = @import("token.zig");
 const Lexer = struct {
     input: []const u8, // for ease of use we'll consider the input to be a string
     position: u16 = 0,
-    read_position: u16 = 1,
+    read_position: u16 = 0,
     ch: u8 = undefined, // this is supposed to be a byte. However, consider the case where we're supporting UTF-8
 
     pub fn readChar(self: *Lexer) void {
@@ -28,17 +28,26 @@ const Lexer = struct {
             ',' => tok = newToken(token.COMMA, self.ch),
             '+' => tok = newToken(token.PLUS, self.ch),
             ':' => tok = newToken(token.COLON, self.ch),
-            0 => tok = newToken(token.ENDMARKER, ""),
+            //it was supposed to be "", empty string, but
+            // I haven't been able to figure it out yet
+            // so for now I'll assume 0
+            // Careful when actually dealing with the number 0
+            0 => tok = newToken(token.ENDMARKER, self.ch), //""),
             else => tok = newToken(token.ERRORTOKEN, self.ch),
         }
+        return tok;
     }
 
     pub fn new(input: []const u8) *Lexer {
-        const l = &Lexer{ .input = input };
+        var l: Lexer = Lexer{ .input = input };
         l.readChar(); // how does this init l with the right params?
-        return l;
+        return &l;
     }
 };
+
+fn newToken(token_type: token.TokenType, ch: u8) token.Token {
+    return token.Token{ .type = token_type, .literal = &[1:0]u8{ch} }; // the .literal part might fail
+}
 
 test "nextToken" {
     const Expected = struct { e_type: token.TokenType, literal: []const u8 };
@@ -54,17 +63,14 @@ test "nextToken" {
         .{ .e_type = token.RPAR, .literal = ")" },
         .{ .e_type = token.COLON, .literal = ":" },
         .{ .e_type = token.COMMA, .literal = "," },
-        .{ .e_type = token.ENDMARKER, .literal = "" },
+        .{ .e_type = token.ENDMARKER, .literal = "0" },
     };
 
     for (tests) |t| {
         const tok = l.nextToken();
 
+        // std.mem.eql is how you compare two strings in zig
         try testing.expect(std.mem.eql(u8, tok.type, t.e_type));
         try testing.expect(std.mem.eql(u8, tok.literal, t.literal));
     }
-}
-
-fn newToken(token_type: token.TokenType, ch: u8) token.Token {
-    return token.Token{ .type = token_type, .literal = ch }; // the .literal part might fail
 }
