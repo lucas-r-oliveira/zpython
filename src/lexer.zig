d9c8f4ec41929900cca193029df3a3953b3baee7
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2024-08-31 23:52:23 +0100

    wip fix: new function not being properly used and causing crashes

--- src/lexer.zig
+++ src/lexer.zig
@@ -2,25 +2,43 @@ const std = @import("std");
 const testing = std.testing;
 const token = @import("token.zig");
 
-const Lexer = struct {
+pub const Lexer = struct {
     input: []const u8, // for ease of use we'll consider the input to be a string
-    position: u16 = 0,
-    read_position: u16 = 0,
+    position: u8 = 0,
+    read_position: u8 = 0,
     ch: u8 = undefined, // this is supposed to be a byte. However, consider the case where we're supporting UTF-8
 
+    pub fn new(input: []const u8) *Lexer {
+        var l: Lexer = Lexer{ .input = input, .position = 0, .read_position = 0 };
+        l.readChar();
+        // print here crashes the program???
+        return &l;
+    }
+
     pub fn readChar(self: *Lexer) void {
-        if (self.read_position >= self.input.len) {
+        const input = self.input[0..];
+        if (self.read_position >= input.len) {
             self.ch = 0;
         } else {
-            self.ch = self.input[self.read_position];
+            // this works
+            self.ch = input[self.read_position];
         }
+        // the problem has to be here
+        // when i comment this my
+        // terminal turns into demon mode
         self.position = self.read_position;
         self.read_position += 1;
+        // print here works just fine
     }
 
     pub fn nextToken(self: *Lexer) token.Token {
+        // this one already doesn't work
+        // it looks like pos and read pos have garbage
+
         var tok: token.Token = undefined;
 
+        //this prints the wrong value
+        //std.debug.print("\nself.ch (nextToken): {c}\n", .{self.ch});
         switch (self.ch) {
             '=' => tok = newToken(token.EQUAL, self.ch),
             '(' => tok = newToken(token.LPAR, self.ch),
@@ -35,13 +53,8 @@ const Lexer = struct {
             0 => tok = newToken(token.ENDMARKER, self.ch), //""),
             else => tok = newToken(token.ERRORTOKEN, self.ch),
         }
+        self.readChar();
         return tok;
-    }
-
-    pub fn new(input: []const u8) *Lexer {
-        var l: Lexer = Lexer{ .input = input };
-        l.readChar(); // how does this init l with the right params?
-        return &l;
     }
 };
 
@@ -49,28 +62,39 @@ fn newToken(token_type: token.TokenType, ch: u8) token.Token {
     return token.Token{ .type = token_type, .literal = &[1:0]u8{ch} }; // the .literal part might fail
 }
 
-test "nextToken" {
-    const Expected = struct { e_type: token.TokenType, literal: []const u8 };
-
-    const input = "=+():,";
-
-    const l = Lexer.new(input);
-
-    const tests = [_]Expected{
-        .{ .e_type = token.EQUAL, .literal = "=" },
-        .{ .e_type = token.PLUS, .literal = "+" },
-        .{ .e_type = token.LPAR, .literal = "(" },
-        .{ .e_type = token.RPAR, .literal = ")" },
-        .{ .e_type = token.COLON, .literal = ":" },
-        .{ .e_type = token.COMMA, .literal = "," },
-        .{ .e_type = token.ENDMARKER, .literal = "0" },
-    };
-
-    for (tests) |t| {
-        const tok = l.nextToken();
-
-        // std.mem.eql is how you compare two strings in zig
-        try testing.expect(std.mem.eql(u8, tok.type, t.e_type));
-        try testing.expect(std.mem.eql(u8, tok.literal, t.literal));
-    }
-}
+//test "nextToken" {
+//    const Expected = struct { e_type: token.TokenType, literal: []const u8 };
+//
+//    const input = "=+():,";
+//
+//    const l = Lexer.new(input);
+//    // print here works just fine
+//
+//    const tests = [_]Expected{
+//        .{ .e_type = token.EQUAL, .literal = "=" },
+//        .{ .e_type = token.PLUS, .literal = "+" },
+//        .{ .e_type = token.LPAR, .literal = "(" },
+//        .{ .e_type = token.RPAR, .literal = ")" },
+//        .{ .e_type = token.COLON, .literal = ":" },
+//        .{ .e_type = token.COMMA, .literal = "," },
+//        .{ .e_type = token.ENDMARKER, .literal = "0" },
+//    };
+//
+//    for (tests) |t| {
+//        std.log.warn("---- NEW TEST ----", .{});
+//        // print here crashes the program???
+//
+//        const tok = l.nextToken();
+//        // also here
+//
+//        // std.mem.eql is how you compare two strings in zig
+//        //try testing.expectEqual(t.e_type, tok.type);
+//        //try testing.expectEqual(t.literal, tok.literal);
+//        //try testing.expectEqua(std.mem.eql(u8, tok.literal, t.literal));
+//
+//        std.log.warn("\nt_literal: {s} | t.e_type: {s} | tok.literal: {s} | tok.type: {s}\n", .{ t.literal, t.e_type, tok.literal, tok.type });
+//
+//        try testing.expectEqualSlices(u8, t.literal, tok.literal);
+//        try testing.expectEqualSlices(u8, t.e_type, tok.type);
+//    }
+//}
--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,28 @@ const token = @import("token.zig");
 const lexer = @import("lexer.zig");
 
 pub fn main() !void {
-    const tok = token.Token{ .type = "sometype", .literal = "=" };
-    std.debug.print("t: {s}, l: {s}\n", .{ tok.type, tok.literal });
+    //const tok = token.Token{ .type = "sometype", .literal = "=" };
+    //std.debug.print("t: {s}, l: {s}\n", .{ tok.type, tok.literal });
+
+    const input = "=+():,";
+    //const l = lexer.Lexer.new(&input);
+    var l = lexer.Lexer{ .input = input };
+    //std.debug.print("\nmain: l.input: {s} | l.pos: {d} | l.read_pos: {d} | l.ch: {c}\n", .{ l.input, l.position, l.read_position, l.ch });
+    l.readChar();
+    //l.readChar();
+    //std.debug.print("\nmain: l.input: {s} | l.pos: {d} | l.read_pos: {d} | l.ch: {c}\n", .{ l.input, l.position, l.read_position, l.ch });
+
+    //TODO: error handling and capture error
+    for (input) |_| {
+        const tok = l.nextToken();
+        std.debug.print("\ntok.type: {s} | tok.literal: {s}", .{ tok.type, tok.literal });
+
+        // this is unreachable => eithr struct is deallocated or fields are not properly
+        // initialized or smth
+        std.debug.print("\nmain: l.input: {s} | l.pos: {} | l.read_pos: {} | l.ch: {c}\n", .{ l.input, l.position, l.read_position, l.ch });
+
+        //std.debug.assert(tok.literal == c);
+    }
 }
 
 //test "simple test" {
